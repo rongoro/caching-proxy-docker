14cef10246a4badacc9c10a241478ae43a365415
Ron Gorodetzky <rongoro@gmail.com>
2017-09-14 10:36:22 -0400

    flesh out run command and move to port 8080

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,4 @@ RUN rm /etc/nginx/sites-enabled/default
 ADD nginx.conf /etc/nginx
 ADD proxy /etc/nginx/sites-enabled
 
-EXPOSE 8888
-
 CMD service nginx start
--- run.sh
+++ run.sh
@@ -3,11 +3,72 @@
 IMAGENAME="caching_proxy_docker"
 
 HASIMAGE=`docker images | grep $IMAGENAME`
+DOCKERLABEL="localhost.job_type=caching_http_proxy"
+PORT=8080
 
-echo $1
+function usage() {
+    echo "Manage an cahing HTTP proxy and http_proxy environment variable"
+    echo "$0 (start|stop|status|info|rebuild)"
+}
 
-if [[ $1 == "rebuild" || ! $HASIMAGE ]] ; then
+function rebuild() {
    docker build -t $IMAGENAME .
+}
+
+function getContainer() {
+    docker ps --filter "label=$DOCKERLABEL" --format "{{.ID}}"
+}
+
+function status() {
+    docker ps --filter "label=$DOCKERLABEL"
+}
+
+function info() {
+    CID=`getContainer`
+    PROXYIP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${CID}`
+    echo "$PROXYIP:$PORT"
+}
+
+function start() {
+    CID=`docker run -d -p $PORT:$PORT -l "$DOCKERLABEL" $IMAGENAME`
+    info
+}
+
+
+function stop() {
+    CID=`getContainer`
+    docker kill $CID
+}
+
+if [[ $# -ne 1 ]] ; then
+    echo "Invalid number of arguments."
+    usage
+    exit 1
 fi
 
-docker run -p 8888:8888 $IMAGENAME
+COMMAND=$1
+
+VALID_ARGS=(start stop rebuild)
+
+
+case "$COMMAND" in
+    "rebuild" )
+        rebuild
+        ;;
+    "start" )
+        start
+        ;;
+    "stop" )
+        stop
+        ;;
+    "status" )
+        status
+        ;;
+    "info" )
+        info
+        ;;
+    *)
+        echo "Invalid Argument: $*"
+        usage
+        exit 1;;
+esac
